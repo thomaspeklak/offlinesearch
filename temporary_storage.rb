0c11ae9285e9747abd55c656e3788000448db00f
tom <tom@b20a1ddf-2037-0410-8d96-e47230229213>
2008-01-08 23:06:17 +0000

    temporal_storage INIT abstraction of the temporary datastorage
    git-svn-id: svn://localhost/offline_search/trunk@30 b20a1ddf-2037-0410-8d96-e47230229213

---
+++ temporary_storage.rb
@@ -0,0 +1,34 @@
+# temporary storage
+# class to store crawled data before the date is written to a file
+# options
+#   datebase
+#     sqlite, mysql
+#
+
+class Temporary_Storage
+  attr_reader :storage_handler
+  def initialize(mode)
+    @storage_handler=case
+      when mode=='sqlite': Sqlite.new('temporal_storage.db')
+    end
+  end
+  
+  private
+  
+  class Sqlite
+    def initialize(db)
+      require 'rubygems'
+      require 'sqlite3'
+      @db = SQLite3::Database.new(db)
+      sql = "
+        create table files ( 
+        filename varchar2(255), 
+        title varchar2(255), 
+        pagerank integer
+        ); "
+      @db.execute_batch(sql)
+    endÅ
+  end
+end
+
+t=Temporal_Storage.new('sqlite')
