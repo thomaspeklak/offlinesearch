cfbef5788d5fa5c2b9ba245ece1098d13f9e916a
tom <tom@b20a1ddf-2037-0410-8d96-e47230229213>
2008-01-09 07:07:58 +0000

    insert and get a row into file storage
    git-svn-id: svn://localhost/offline_search/trunk@32 b20a1ddf-2037-0410-8d96-e47230229213

--- temporary_storage.rb
+++ temporary_storage.rb
@@ -9,8 +9,18 @@ class Temporary_Storage
   attr_reader :storage_handler
   def initialize(mode)
     @storage_handler=case
-      when mode=='sqlite': Sqlite.new('temporal_storage.db')
+      when mode=='sqlite': Sqlite.new('storage.db')
     end
+  end
+  #start pagerank with 1 to enable mutliplicatoin pagerank
+  def store_file(filename,title,pagerank=1)
+    @storage_handler.store_file(filename,title,pagerank)
+  end
+  def get_file(filename)
+    f=@storage_handler.get_file(filename)
+    { 'filename'=>f[0],
+      'titel'=>f[1],
+      'pagerank'=>f[2] }
   end
   
   private
@@ -27,8 +37,18 @@ class Temporary_Storage
         pagerank integer
         ); "
       @db.execute_batch(sql)
-    endÅ
+    end
+    def store_file(filename, title, pagerank)
+      @db.execute( "insert into files values ( ?, ?, ? )", filename, title, pagerank)
+    end
+    def get_file(filename)
+      @db.get_first_row( "select * from files where filename = ?", filename)
+    end
   end
 end
 
-t=Temporal_Storage.new('sqlite')
+t=Temporary_Storage.new('sqlite')
+t.store_file('test.html','Test',20)
+r=t.get_file('test.html')
+puts r.inspect
+
