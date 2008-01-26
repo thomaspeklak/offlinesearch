733d68cae8cc216456e8763674d9d5f299f02200
tom <tom@b20a1ddf-2037-0410-8d96-e47230229213>
2008-01-26 21:54:35 +0000

    refactored search is able to search for multiple terms seperated by a space
    git-svn-id: svn://localhost/offline_search/trunk@73 b20a1ddf-2037-0410-8d96-e47230229213

--- templates/base/search.js
+++ templates/base/search.js
@@ -14,7 +14,23 @@ $(document).ready(function(){
 });
 
 (function($){
-	$.fn.show_results = function(term){
+	$.fn.show_results = function(searchValue){
+		var searchTerms = searchValue.split(' ');
+		var results=new Array();
+		for (term in searchTerms){
+			if (results.length)
+				results = $.intersectResultsArrays(results,$.getResultsForTerm(searchTerms[term]))
+			else
+				results = $.getResultsForTerm(searchTerms[term]);
+		}
+		results.sort($.sortByFirstValue);
+		output= new Array();
+		while(r = results.shift())
+			output.push('<li>'+r[0]+'<a href="'+r[1]+'">'+r[2]+'</a>');
+		this.html('<ol>'+output.join('')+'</ol>');
+	};
+
+	$.getResultsForTerm = function(term){
 		var foundTerms = new Array();
 		var searchTerm=eval('/'+term+'/');
 		for(t in terms)
@@ -31,16 +47,8 @@ $(document).ready(function(){
 					results[id]=[terms[foundTerms[t]][docs][1]*file[2],file[1],file[0]];
 			}
 		}
-		results.sort($.sortByFirstValue);
-		output= new Array();
-		while(r = results.shift())
-			output.push('<li>'+r[0]+'<a href="'+r[1]+'">'+r[2]+'</a>');
-		this.html('<ol>'+output.join('')+'</ol>');
-	};
-	$.get_matches = function (term) {
-		
-	};
-	
+		return results;
+	}
 	$.sortByFirstValue = function(a,b){return b[0]-a[0];};
 	$.intersectResultsArrays = function (result1,result2){
 		intersectedResults = new Array();
