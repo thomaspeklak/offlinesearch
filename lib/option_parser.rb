7ccbd87d009420ea98546238a95ddda99276a7c9
tom <tom@b20a1ddf-2037-0410-8d96-e47230229213>
2008-02-10 21:30:00 +0000

    more refactoring
    git-svn-id: svn://localhost/offline_search/trunk@97 b20a1ddf-2037-0410-8d96-e47230229213

--- lib/option_parser.rb
+++ lib/option_parser.rb
@@ -21,15 +21,16 @@ OptionParser.new do |opts|
   opts.separator "Optional arguments"
   opts.separator "can also be specified in the config file"
   opts.separator "command line arguments will overwrite any given value in the config file"
-  opts.on('-d', '--docpath=[DOCPATH]', String,'path of the documents') do |d|
+  opts.on('-d', '--docpath=DOCPATH', String,'path of the documents') do |d|
     $config['crawler']['docpath'] = d
   end 
-  opts.on('-f', '--search-data-file=[SEARCH_DATA_FILE]', String,'path and name of the search data file') do |f|
+  opts.on('-f', '--search-data-file=SEARCH_DATA_FILE', String,'path and name of the search data file') do |f|
     $config['search_generator']['search_data_file'] = f
-
   end
-  opts.on('-s', '--stopword-list=[STOPWORD_LIST]', String,'stopword list, if none is specified the default stop word list is used') do |s|
+  opts.on('-s', '--stopword-list=STOPWORD_LIST', String,'stopword list, if none is specified the default stop word list is used') do |s|
+    puts $config['crawler']['stopwords']
     $config['crawler']['stopwords'] = s
+    puts $config['crawler']['stopwords']
   end
   opts.separator ""
   opts.on_tail('-h','--help','Show this message') do
@@ -41,3 +42,7 @@ OptionParser.new do |opts|
     exit
   end
 end.parse!
+
+if ($config['crawler']['stopwords'].nil?) then
+  $config['crawler']['stopwords'] = File.dirname(__FILE__) +'/stopwords.txt'
+end
