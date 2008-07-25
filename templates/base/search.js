635652d88497beef6cdc3a35fd65d5424925396a
tom <tom@b20a1ddf-2037-0410-8d96-e47230229213>
2008-07-25 14:02:35 +0000

    git-svn-id: svn://localhost/offline_search/trunk@131 b20a1ddf-2037-0410-8d96-e47230229213

--- templates/base/search.js
+++ templates/base/search.js
@@ -14,13 +14,10 @@ $(document).ready(function(){
 });
 
 (function($){
-	$.lang = {
-		no_entries: 'Keine Einträge gefunden.'
-	};
 	$.fn.show_results = function(searchValue){
-		if(searchValue.match(/["'].*[^a-zA-ZÄÖÜäöüß].*["']/))
+		if(searchValue.match(/["'].*[ _\-\(\)].*["']/))
 			searchValue = $.quoteSearch(searchValue);
-		var searchTerms = searchValue.split(/[^a-zA-ZÄÖÜäöüß]/);
+		var searchTerms = searchValue.split(/[^a-zA-Z0-9������]/);
 		var results=[];
 		for (var term in searchTerms){
 			if(searchTerms[term].length > 1){
@@ -32,15 +29,9 @@ $(document).ready(function(){
 		}
 		results.sort($.sortByFirstValue);
 		var output = [];
-		if(results.length){
-			var r;
-			while(r = results.shift())
-				output.push('<li><a href="'+rel_path+r[1]+'">'+r[2]+'</a>');
-			this.html('<ol>'+output.join('')+'</ol>');
-		}
-		else{
-			this.html('<p><strong>'+$.lang.no_entries+'</strong></p>');
-		}
+		while(r = results.shift())
+			output.push('<li><a href="'+rel_path+r[1]+'">'+r[2]+'</a>');
+		this.html('<ol>'+output.join('')+'</ol>');
 	};
 	$.getResultsForTerm = function(term){
 		var exact_term = /^["'][^"']+["']$/.test(term);
@@ -81,11 +72,11 @@ $(document).ready(function(){
 			}
 		}
 		return results;
-	}
-
+	}	
+	
 	$.sortByFirstValue = function(a,b){return b[0]-a[0];};
 	$.intersectResultsArrays = function (result1,result2){
-		var intersectedResults = [];
+		intersectedResults = [];
 		for (var r1 in result1)
 			if(result2[r1]) intersectedResults[r1] = [result1[r1][0]+result2[r1][0], result2[r1][1], result2[r1][2]];
 		return intersectedResults;
@@ -95,15 +86,14 @@ $(document).ready(function(){
 			if(lesser[r]) greater[r][0]+=lesser[r][0];
 		return greater;
 	}
-
+	
 	$.quoteSearch = function(value){
-		var quotes;
-		while(quotes = value.match(/["'](.*[^"'][^'"a-zA-ZÄÖÜäöüß][^"'].*)["']/))
+		while(quotes = value.match(/["'](.*[^"'][ _\-\(\)][^"'].*)["']/))
 			{
-				var terms = quotes[1].split(/[^'"a-zA-ZÄÖÜäöüß]/);
-				value = value.replace(/["'].*[^"'][^'"a-zA-ZÄÖÜäöüß][^"'].*["']/,"'"+terms.join("' '")+"'");
+				var terms = quotes[1].split(/[ _\-\(\)]/);
+				value = value.replace(/["'].*[^"'][ _\-\(\)][^"'].*["']/,"'"+terms.join("' '")+"'");
 			}
-		return value.replace(/''/g,' ').replace(/ +/,' ');
+		return value;
 	};
-
+	
 })(jQuery);
