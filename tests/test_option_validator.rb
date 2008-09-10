06c4d8c0f7f3a91641ea6a9908dbb6bc5e43a12c
Thomas Peklak <thomas.peklak@gmail.com>
2008-09-10 21:13:22 +0200

    added test for option validator

---
+++ tests/test_option_validator.rb
@@ -0,0 +1,17 @@
+$: << File.expand_path(File.dirname(__FILE__)+'/../lib')
+
+
+require 'test/unit' 
+require 'option_validator'
+
+class TestOptionValidator < Test::Unit::TestCase 
+  def setup
+    $config = {'logger'=> { 'file' => STDOUT, 'level' => 'INFO'}}
+    require "log_init"
+    require 'yaml'
+    $config = YAML.load_file(File.expand_path(File.dirname(__FILE__)+'/../tests/test_files/ov_config.yaml'))
+  end
+  def test_standard
+    OptionValidator.new()
+  end
+end
